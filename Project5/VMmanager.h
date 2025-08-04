d4cf779f46d9b3c4b3dc006dea920de049bb019e
Kian <kianjk@outlook.com>
2025-08-03 23:35:53 -0700

    Update VMmanager.h

--- Project5/VMmanager.h
+++ Project5/VMmanager.h
@@ -1,5 +1,5 @@
 #ifndef VMMANAGER_H
-#define VMANAGER_H
+#define VMMANAGER_H
 
 #define PAGE_SIZE 4096
 #define NUM_PAGES 50
@@ -7,10 +7,10 @@
 #define MAX_PROCESSES 20
 #define TLB_SIZE 8
 
-extern int tlb_hits ;
+extern int tlb_hits;
 extern int tlb_misses;
 extern int process_count;
-extern int fifo_queue[NUM_FRAMES];
+extern int fifo_queue[];
 extern int fifo_front;
 extern int fifo_rear;
 
@@ -45,8 +45,24 @@ extern Frame frames[NUM_FRAMES];
 extern Process processes[MAX_PROCESSES];
 extern TLBEntry tlb[TLB_SIZE];
 
+void enqueue_fifo(int frame_index);
+int dequeue_fifo();
 void initialize();
+void initialize_page_table(Process *process);
 int create_process();
+int allocate_frame();
+void simulate_disk_io();
+void log_page_fault(int, int, const char*);
+int tlb_lookup(int, int*);
+void tlb_add_entry(int, int);
+void load_page(Process*, int, int);
+void free_frames(Process *process);
+void print_tlb_state();
+void access_memory(Process*, int, int, char);
+void free_process(int vm_pid);
+void print_memory_state();
+
+#endif
 void access_memory(Process *process, int process_id, int virtual_address, char mode);
 void free_process(int vm_pid);
 void print_memory_state();
